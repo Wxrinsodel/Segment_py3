4c01a30422aa8af1410a672f5e64132139ea89fe
Supawadee <150314053+Wxrinsodel@users.noreply.github.com>
2025-01-23 10:46:41 +0700

    3 finish

--- matplot_inclass.py
+++ matplot_inclass.py
@@ -3,13 +3,17 @@ import numpy as np
 
 
 x = np.linspace(-10, 10, 100)  
-f_x = np.arctan(x)     # f(x) = arctan
+f_x1 = x**2        # f(x) = x^2 
+f_x2 = x * np.sin(2 * x)  # f(x) = x * sin(2*x)
+f_x3 = np.arctan(x)     # f(x) = arctan
 
 #
-plt.plot(x, f_x, label='f(x) = arctg(x)', color='red')
+plt.plot(x, f_x1, label='f(x) = x^2 ', color='green')
+plt.plot(x, f_x2, label='f(x) = x * sin(2*x)', color='red')
+plt.plot(x, f_x3, label='f(x) = arctg(x)', color='Blue')
 
 
-plt.title('Graphs of f(x) = arctg(x)')
+plt.title('Graphs of f(x) = x^2, f(x) = x * sin(2*x), and f(x) = arctg(x)')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.legend()
