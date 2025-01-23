c081a2c3da8f88af07980085bdacf1dd5ee83818
Supawadee <150314053+Wxrinsodel@users.noreply.github.com>
2025-01-23 10:39:24 +0700

    b finish

--- matplot_inclass.py
+++ matplot_inclass.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 x = np.linspace(-10, 10, 100)  
-f_x = x**2          # f(x) = x^2 
+f_x = x * np.sin(2 * x)       # f(x) = x * sin(2*x)
 
-plt.plot(x, f_x, label='f(x) = x^2', color='blue')
+#
+plt.plot(x, f_x, label='f(x) = x * sin(2*x)', color='red')
 
-plt.title('Graphs of f(x) = x^2')
+
+plt.title('Graphs of f(x) = x * sin(2*x)')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.legend()
