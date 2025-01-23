e96d79dc52bbc14a587db93dc692f20f4931753d
Supawadee <150314053+Wxrinsodel@users.noreply.github.com>
2025-01-23 10:42:44 +0700

    c finish

--- matplot_inclass.py
+++ matplot_inclass.py
@@ -3,13 +3,13 @@ import numpy as np
 
 
 x = np.linspace(-10, 10, 100)  
-f_x = x * np.sin(2 * x)       # f(x) = x * sin(2*x)
+f_x = np.arctan(x)     # f(x) = arctan
 
 #
-plt.plot(x, f_x, label='f(x) = x * sin(2*x)', color='red')
+plt.plot(x, f_x, label='f(x) = arctg(x)', color='red')
 
 
-plt.title('Graphs of f(x) = x * sin(2*x)')
+plt.title('Graphs of f(x) = arctg(x)')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.legend()
